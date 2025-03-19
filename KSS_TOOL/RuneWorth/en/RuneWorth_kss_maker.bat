2c752c5bcebee2281164549b11e7aece08d67276
uniskie <uniuniunih@gmail.com>
2025-03-19 14:38:50 +0900

    add KSS_TOOL/RuneWorth/en

--- KSS_TOOL/RuneWorth/en/RuneWorth_kss_maker.bat
+++ KSS_TOOL/RuneWorth/en/RuneWorth_kss_maker.bat
@@ -8,18 +8,18 @@ SET KSSPLAYER=RuneWorth_kss_head
 
 ECHO ***** for Rune Worth (MSX2) ****************************************
 ECHO.
-ECHO ** "%KSSFILE%" を作成 します
+ECHO ** make the "%KSSFILE%" 
 ECHO.
-ECHO 準備：
-ECHO    RuneWorth_S.dsk ... スタートディスク ディスクイメージ
-ECHO    RuneWorth_A.dsk ... ゲームディスク A ディスクイメージ
-ECHO    RuneWorth_B.dsk ... ゲームディスク B ディスクイメージ
+ECHO Required:
+ECHO    RuneWorth_S.dsk ... Start Disk disk-image
+ECHO    RuneWorth_A.dsk ... Game Disk A disk-image
+ECHO    RuneWorth_B.dsk ... Game Disk B disk-image
 ECHO.
-ECHO    bincut2.exe     ... バイナリーカッター
+ECHO    bincut2.exe     ... Binary Cutter
 ECHO      https://nezplug.sourceforge.net/
 ECHO      https://www.purose.net/befis/download/nezplug/oldsite.shtml
 ECHO.
-ECHO    AILZ80ASM.exe   ... Z80アセンブラ―
+ECHO    AILZ80ASM.exe   ... Z80 Assembler
 ECHO      https://github.com/AILight/AILZ80ASM
 ECHO.
 ECHO ********************************************************************
@@ -31,7 +31,7 @@ IF NOT EXIST RuneWorth_B.dsk call :no_disk_b
 IF NOT EXIST bincut2.exe call :no_bincut2
 
 IF %ER%==0 GOTO :CHECK_OK
-ECHO *** 処理を中断します。
+ECHO *** Aborted.
 GOTO :WAIT
 
 REM ************************************************
@@ -39,41 +39,41 @@ REM  Display Error
 REM ************************************************
 :NO_DISK_S
 SET ER=1
-ECHO [ERROR] RuneWorth_S.dsk ... スタートディスク ディスクイメージ がありません。
+ECHO [ERROR] RuneWorth_S.dsk ... Start Disk disk-image is not found.
 GOTO :EXIT_B
 
 :NO_DISK_A
 SET ER=1
-ECHO [ERROR] RuneWorth_A.dsk ... ゲームディスク A ディスクイメージ がありません。
+ECHO [ERROR] RuneWorth_A.dsk ... Game Disk A disk-image is not found.
 GOTO :EXIT_B
 
 :NO_DISK_B
 SET ER=1
-ECHO [ERROR] RuneWorth_B.dsk ... ゲームディスク B ディスクイメージ がありません。
+ECHO [ERROR] RuneWorth_B.dsk ... Game Disk B disk-image is not found.
 GOTO :EXIT_B
 
 :NO_BINCUT2
 SET ER=1
-ECHO [ERROR] bincut2.exe がありません。
+ECHO [ERROR] bincut2.exe is not found.
 GOTO :EXIT_B
 
 :NO_ASM
 SET ER=1
-ECHO [ERROR] AILZ80ASM.exe がありません。
-ECHO *** 処理を中断します。
+ECHO [ERROR] AILZ80ASM.exe is not found.
+ECHO *** Aborted.
 GOTO :WAIT
 
 REM ************************************************
 :check_ok
 
 REM ************************************************
-REM KSSヘッダ（コールエントリ）のアセンブル
+REM Assemble the KSS header. (driver controller)
 REM ************************************************
 IF "%FORCE_ASM%"=="1" GOTO :exec_assemble
 IF EXIST "%KSSPLAYER%.bin" GOTO skip_asm_player
 
 :exec_assemble
-ECHO * "%KSSPLAYER%.bin" をアセンブルします。
+ECHO * Assembling "%KSSPLAYER%.bin".
 IF NOT EXIST AILZ80ASM.exe GOTO :no_asm
 REM AILZ80ASM %KSSPLAYER%.asm -f -bin "%KSSPLAYER%.bin" -sm minimal-equ -sym "%KSSPLAYER%.sym" -lst "%KSSPLAYER%.lst" -err "%KSSPLAYER%.err" -gap 0
 AILZ80ASM %KSSPLAYER%.asm -f -bin "%KSSPLAYER%.bin" -gap 0
@@ -82,7 +82,7 @@ IF ERRORLEVEL 1 GOTO :exit_b
 :skip_asm_player
 
 REM ************************************************
-ECHO * バイナリデータを切り出します。
+ECHO * Extracts binary data.
 REM ************************************************
 
 REM *** OPLLDRV (RAM:0100-1EFF)
@@ -93,12 +93,12 @@ REM *** PSGDRV (RAM:0100-1EFF)
 REM +7EBD size:1E00
 BINCUT2 -s 07EBD -l 01E00 -o RuneWorth.PSG RuneWorth_S.DSK
 
-REM *** OPENNING MUSIC
+REM *** OPENNING MUSIC (SYSTEM DISK)
 REM SECTOR 46-51
 REM +8C00-A3FF size:01800
 BINCUT2 -s 08C00 -l 01800 -o RuneWorth_OP.MUS RuneWorth_S.DSK
 
-REM *** ENDING MUSIC
+REM *** ENDING MUSIC (SYSTEM DISK)
 REM SECTOR 110-11C
 REM +22000-237FF size:01800
 BINCUT2 -s 22000 -l 01800 -o RuneWorth_ED.MUS RuneWorth_S.DSK
@@ -108,7 +108,7 @@ REM SECTOR 1DC-213
 REM +3B800-427FF size:07000
 BINCUT2 -s 3B800 -l 07000 -o RuneWorth_C.MUS RuneWorth_A.DSK
 
-REM *** COMMON MUSIC 2 (GAME DISK A/B) #宝箱 #危険がアブナイ
+REM *** COMMON MUSIC 2 (GAME DISK A/B) #tresurebox #Look out, it's Dangerous
 REM SECTOR 2-4
 REM +00400-00800 size:0400
 BINCUT2 -s 00400 -l 00400 -o RuneWorth_C2.MUS RuneWorth_A.DSK
@@ -124,12 +124,12 @@ REM +4CA00-51FFF size:05600
 BINCUT2 -s 4CA00 -l 05600 -o RuneWorth_B.MUS RuneWorth_B.DSK
 
 REM ********************************
-ECHO * バイナリデータを結合します。
+ECHO * Combine binary data.
 REM ********************************
 COPY /B %KSSPLAYER%.BIN+RuneWorth.OPL+RuneWorth.PSG+RuneWorth_OP.MUS+RuneWorth_C.MUS+RuneWorth_C2.MUS+RuneWorth_A.MUS+RuneWorth_B.MUS+RuneWorth_ED.MUS "%KSSFILE%">nul
 
 REM ********************************
-ECHO * 作業ファイルを片付けます。
+ECHO * Clean up working files.
 REM ********************************
 DEL RuneWorth.PSG>nul
 DEL RuneWorth.OPL>nul
@@ -141,7 +141,7 @@ DEL RuneWorth_A.MUS>nul
 DEL RuneWorth_B.MUS>nul
 
 REM ********************************
-ECHO * 終了ました。
+ECHO * Finished.
 REM ********************************
 :WAIT
 PAUSE
